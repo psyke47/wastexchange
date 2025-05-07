467dd5f555f7eb46d79ec970e1fefb18eaff624d
dakalomunonde <86750102+psyke47@users.noreply.github.com>
2025-05-07 08:58:18 +0200

    Add Navbar component and update App to use it

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div>
+                <div>
+                    {/* Logo */}
+                    <div>Logo</div>
+
+                    {/* Desktop Menu */}
+                    <div>Desktop menu</div>
+
+                    {/* Auth btn */}
+                    <div>Auth btn</div>
+
+                    {/* Mobile Menu */}
+                    <div>Mobile menu</div>
+                </div>
+            </div>
+        </nav>
+    )
+}
