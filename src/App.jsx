3661234c91609b7900fb27091c50dfebf1c75098
sapho <saphotiki@gmail.com>
2025-05-07 11:36:34 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
+    <>
     <Navbar />
+    
+    <HeroSection />
+    </>
   )
 }
 export default App
